392660c6ff80f755132b2e9bcb0eb22cf2b8ef29
frittatelle <myfavouritefreakelle@gmail.com>
2021-03-31 22:58:25 +0200

    define credentials with file, not CLI

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,18 +32,20 @@ if [ -n "$AWS_S3_ENDPOINT" ]; then
   ENDPOINT_APPEND="--endpoint-url $AWS_S3_ENDPOINT"
 fi
 
-# Create a dedicated profile for this action to avoid conflicts
-# with past/future actions.
-# https://github.com/jakejarvis/s3-sync-action/issues/1
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-${AWS_ACCESS_KEY_ID}
-${AWS_SECRET_ACCESS_KEY}
-${AWS_SESSION_TOKEN}
-${AWS_REGION}
-text
+# Create a crdedentials file with session token
+cat <<EOF >> ~/.aws/credentials
+[s3-sync-action]
+aws_access_key_id=${AWS_ACCESS_KEY_ID}
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+aws_session_token=${AWS_SESSION_TOKEN}
+region
+EOF
+# config file
+cat <<EOF >> ~/.aws/config
+[profile s3-sync-action]
+region=${AWS_REGION}
 EOF
 
-cat ~/.aws/credentials
 
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
@@ -56,10 +58,14 @@ sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
 # We need to re-run `aws configure` with bogus input instead of
 # deleting ~/.aws in case there are other credentials living there.
 # https://forums.aws.amazon.com/thread.jspa?threadID=148833
-aws configure --profile s3-sync-action <<-EOF > /dev/null 2>&1
-null
-null
-null
-null
-text
+cat <<EOF >> ~/.aws/credentials
+[s3-sync-action]
+aws_access_key_id=
+aws_secret_access_key=
+aws_session_token=
+EOF
+# config file
+cat <<EOF >> ~/.aws/config
+[profile s3-sync-action]
+region=
 EOF
