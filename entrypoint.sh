b4a79b8654dc88d20a1bbc630feb6eedec940cc1
frittatelle <myfavouritefreakelle@gmail.com>
2021-04-01 11:51:01 +0200

    comments clean up

--- entrypoint.sh
+++ entrypoint.sh
@@ -56,9 +56,6 @@ sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               ${ENDPOINT_APPEND} $*"
 
 # Clear out credentials after we're done.
-# We need to re-run `aws configure` with bogus input instead of
-# deleting ~/.aws in case there are other credentials living there.
-# https://forums.aws.amazon.com/thread.jspa?threadID=148833
 cat <<EOF >> ~/.aws/credentials
 [s3-sync-action]
 aws_access_key_id=
